58a382d0497ff23da5a85dfe5ee78f95b9e13f20
Udit <udit@eskaytech.com>
2023-03-28 00:41:25 +0530

    Include RPC path on the web server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import fastify from 'fastify'
+import axios from 'axios'
 import { ethers } from "ethers";
+
+const HARDHAT_URL = 'http://0.0.0.0:8545'
 
 const server = fastify()
 const provider = new ethers.providers.JsonRpcProvider('http://0.0.0.0:8545')
@@ -8,8 +11,18 @@ server.get('/block-number', async (request, reply) => {
   reply.send(await provider.getBlockNumber())
 })
 
-server.get('/rpc-url', async (request, reply) => {
-  reply.send(process.env.RPC_URL)
+server.get('/rpc', async (request, reply) => {
+  reply.send('Method does not exist. Use POST for /rpc to connect with ' + process.env.RPC_URL)
+})
+
+server.post('/rpc', async (request, reply) => {
+  try {
+    const { data } = await axios.post(HARDHAT_URL, request.body);
+
+    reply.send(data);
+  } catch (err: any) {
+    reply.status(500).send({ error: err.message });
+  }
 })
 
 server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
